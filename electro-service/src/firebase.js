4eb2eef132341b87c76b2ef4922b697b624c5157
Lyudov <dlyudov@gmail.com>
2023-11-14 20:15:19 +0200

    Add Project to Github

---
+++ electro-service/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDca94NjzMOvH1U5gn_RVYTIRUUFJC4QUQ",
+    authDomain: "electroserviceproject.firebaseapp.com",
+    projectId: "electroserviceproject",
+    storageBucket: "electroserviceproject.appspot.com",
+    messagingSenderId: "262792394744",
+    appId: "1:262792394744:web:01c781a74207b184c0880d"
+};
+
+const app = initializeApp(firebaseConfig);
