3737d1ea7405fa43614f79b8b88537faae158230
Lyudov <dlyudov@gmail.com>
2023-11-14 20:25:47 +0200

    Create Test Component

---
+++ electro-service/src/Test.jsx
@@ -0,0 +1,12 @@
+function Test() {
+  return (
+    <div>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" />
+      <label htmlFor="phoneNumber">Phone Number</label>
+      <input type="text" name="phoneNumber" />
+    </div>
+  );
+}
+
+export default Test;
