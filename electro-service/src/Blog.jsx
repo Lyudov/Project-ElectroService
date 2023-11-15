cdd8d536ddbe3a24663495306b5d252273bde7f5
Lyudov <dlyudov@gmail.com>
2023-11-16 00:28:31 +0200

    Create Blog data service

--- electro-service/src/Blog.jsx
+++ electro-service/src/Blog.jsx
@@ -1,4 +1,23 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 function Blog() {
+  const [blogData, setBlogData] = useState([]);
+
+  useEffect(() => {
+    const database = getDatabase();
+    const blogRef = ref(database, "blogService");
+
+    onValue(blogRef, (snapshot) => {
+      const blogServiceData = snapshot.val();
+
+      if (blogServiceData) {
+        const blogServiceList = Object.values(blogServiceData);
+        setBlogData(blogServiceList);
+      }
+    });
+  }, []);
+
   return (
     <section className="blog_section layout_padding">
       <div className="container">
@@ -7,36 +26,19 @@ function Blog() {
           <img src="images/plug.png" alt="" />
         </div>
         <div className="row">
-          <div className="col-md-6">
-            <div className="box">
-              <div className="img-box">
-                <img src="images/blog1.jpg" alt="" />
-              </div>
-              <div className="detail-box">
-                <h5>Blog Title Goes Here</h5>
-                <p>
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomised
-                </p>
+          {blogData.map((data, index) => (
+            <div className="col-md-6" key={index}>
+              <div className="box">
+                <div className="img-box">
+                  <img src={data.imageUrl} alt="" />
+                </div>
+                <div className="detail-box">
+                  <h5>{data.title}</h5>
+                  <p>{data.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="box">
-              <div className="img-box">
-                <img src="images/blog2.jpg" alt="" />
-              </div>
-              <div className="detail-box">
-                <h5>Blog Title Goes Here</h5>
-                <p>
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomised
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
